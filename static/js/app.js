b4ef96f21d81d2b23c33320b217e0b50b03a5492
Sam Alba <sam.alba@gmail.com>
2014-11-28 21:48:26 -0800

    implemented basic URL routing on the UI

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,20 @@
-var postsApp = angular.module('postsApp', []);
+var postsApp = angular.module('postsApp', ['ngRoute']);
+
+postsApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+          when('/', {
+            templateUrl: 'html/index.html',
+            controller: 'IndexCtrl'
+          }).
+          otherwise({redirectTo: '/'});
+    }]);
 
 postsApp.controller('NavbarCtrl', function($scope, $http) {
     $http.get('/api/user').success(function(data) {
         $scope.user = data;
     });
+});
+
+postsApp.controller('IndexCtrl', function($scope) {
 });
