bc9fb8c751cf9cadfd5a91abe040d619972e6a15
Sam Alba <sam.alba@gmail.com>
2014-11-29 09:03:39 -0800

    Code cleaning + switching to ui-router

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,39 @@
-var postsApp = angular.module('postsApp', ['ngRoute']);
+var postsApp = angular.module('postsApp', ['ui.router'])
+    .run(['$rootScope', '$state', '$stateParams',
+            function($rootScope, $state, $stateParams) {
+                // Make the state accessible from the root scope
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ]);
 
-postsApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-          when('/', {
-            templateUrl: 'html/index.html',
-            controller: 'IndexCtrl'
-          }).
-          otherwise({redirectTo: '/'});
+postsApp.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider
+            .otherwise('/');
+
+        var views = {
+            'navbar': {
+                templateUrl: 'html/navbar.html',
+                controller: 'NavbarCtrl'
+            },
+            'menu': {
+                templateUrl: 'html/menu.html',
+                controller: 'MenuCtrl'
+            }
+        };
+        $stateProvider
+            .state('index', {
+                url: '/',
+                views: angular.extend({}, views, {'main': {
+                    templateUrl: 'html/index.html'
+                }})})
+            .state('all-teams', {
+                url: '/all-teams',
+                views: angular.extend({}, views, {'main': {
+                    templateUrl: 'html/all-teams.html'
+                }})});
     }]);
 
 postsApp.controller('NavbarCtrl', function($scope, $http) {
@@ -16,5 +42,7 @@ postsApp.controller('NavbarCtrl', function($scope, $http) {
     });
 });
 
-postsApp.controller('IndexCtrl', function($scope) {
+postsApp.controller('MenuCtrl', function($scope, $location) {
+    $scope.state = $scope.$root.$state.current.name;
+    window.d = $scope.$root;
 });
